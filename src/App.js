31e18c6887c727a94075fa37239cd2c3cf2abff4
maysvx <19214290014@academico.iesb.br>
2022-03-22 22:17:03 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Menu from './components/Menu';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <Menu></Menu>
+
+      <h1>Hello word</h1>
+
+      <Button variant="primary">Primary</Button>{' '}
+
     </div>
   );
 }
---
+++ src/components/Menu.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Menu = () => {
+  return (
+    <div>
+        <ul>
+            <li><a href=''>pagina 01 </a></li>
+            <li><a href=''>pagina 02 </a></li>
+            <li><a href=''>pagina 03 </a></li>
+        </ul>
+
+    </div>
+  )
+}
+
+export default Menu
